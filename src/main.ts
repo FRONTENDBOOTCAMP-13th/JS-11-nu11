f88bd13235e085eac5632cc039f7c750a244db80
박상희 <9011155@naver.com>
2025-05-13 14:45:51 +0900

    🎉 init: 초기 셋팅

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,14 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
+  <div class="bg-amber-300">
     <a href="https://vite.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
+    <h1>Vite + TypeScript22</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>

8b9e4ca44be0ea8247631b9c4bd7df97a5499469
박상희 <9011155@naver.com>
2025-05-13 17:39:57 +0900

    🎉 init:  기능별 폴더 및 파일추가

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         index: "index.html", // 기본 index.html
-        // list: 'src/pages/todo/list.html', // 추가 HTML 파일
+        main: "src/pages/main/index.html", // 추가 HTML 파일
         // regist: 'src/pages/todo/regist.html', // 추가 HTML 파일
         // info: 'src/pages/todo/info.html', // 추가 HTML 파일
         // 필요한 다른 HTML 파일을 여기에 추가

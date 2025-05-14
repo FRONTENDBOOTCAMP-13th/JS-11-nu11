5dd7e119ff1f2e58ed9b10e939723867c5485ce7
Dragon Five <dyddh4842@naver.com>
2025-05-15 00:15:18 +0900

    feat: cleaning main page init

--- src/pages/cleaning/cleaning.ts
+++ src/pages/cleaning/cleaning.ts
@@ -1,1 +1,37 @@
 import "./style.css";
+
+function goRulePage(): void {
+  const btn = document.getElementById("ruleBtn");
+
+  if (btn) {
+    btn.addEventListener("click", () => {
+      window.location.href = "rule.html";
+    });
+  }
+}
+
+function returnMainPage(): void {
+  const btn = document.getElementById("returnMainPageBtn");
+
+  if (btn) {
+    btn.addEventListener("click", () => {
+      window.location.href = "../../../index.html";
+    });
+  }
+}
+
+function goGamePage(): void {
+  const btn = document.getElementById("gameStartBtn");
+
+  if (btn) {
+    btn.addEventListener("click", () => {
+      window.location.href = "game.html";
+    });
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  goRulePage();
+  returnMainPage();
+  goGamePage();
+});
